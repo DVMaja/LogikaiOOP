d7223978da27e49e5c9ac872064098ef35954783
Hidy97 <csaladi.alexandra@gmail.com>
2024-01-08 11:23:23 +0100

    2-ik feladat: Bekeres() javítások

--- src/logikaioop/main.java
+++ src/logikaioop/main.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
+        feladat();
+        
+    }
+
+    private static void feladat() {
         Bekeres beker = new Bekeres();
         beker.kiir();
     }
