f7eff17f33610e4ca1cc94203e8baac631c8648e
Hidy97 <csaladi.alexandra@gmail.com>
2024-01-08 09:43:02 +0100

    Allitasok osztály létrehozása

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -1,14 +1,17 @@
 package logikaioop;
 
+import java.util.Scanner;
+
 public class LogikaiOOP {
 
     public static void main(String[] args) {
-        System.out.println("Leírás");
+        //System.out.println("Leírás");
         System.out.println("Az 1. állítás!");
         System.out.println("Az 2. állítás!");
         System.out.println("Az 3. állítás!");
-        System.out.println("Melyik:");
-        System.out.println("A válasz jó|rossz");
+        //System.out.println("Melyik:");
+        //scr.nextInt();
+        //System.out.println("A válasz jó|rossz");
     }
     
 }
