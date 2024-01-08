ea757b3439d9e0899c07d655e2f8ee3a38cff62c
DVMaja <majadreilinger@gmail.com>
2024-01-08 09:45:25 +0100

    Allitas osztály létrehozva

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class LogikaiOOP {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Leírás");
-        System.out.println("Az 1. állítás!");
-        System.out.println("Az 2. állítás!");
-        System.out.println("Az 3. állítás!");
-        System.out.println("Melyik:");
-        sc.nextInt();
-        System.out.println("A válasz jó|rossz");
+        //Scanner sc = new Scanner(System.in);
+        Allitas allitas1 = new Allitas("Az 1. állítás!");
+        Allitas allitas2 = new Allitas("Az 2. állítás!");
+        Allitas allitas3 = new Allitas("Az 3. állítás!");
+        //System.out.println("Leírás");
+        //System.out.println();
+        System.out.println(allitas1.toString());
+        System.out.println(allitas2.toString());
+        System.out.println(allitas3.toString());
+        //System.out.println("Melyik:");
+        //sc.nextInt();
+        //System.out.println("A válasz jó|rossz");
     }
 
 }
