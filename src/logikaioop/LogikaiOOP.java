ad8e795905a54b22296c91a06743fa2df34f38dd
DVMaja <majadreilinger@gmail.com>
2024-01-08 09:23:12 +0100

    Scanner-rel adatbekérés

--- src/logikaioop/LogikaiOOP.java
+++ src/logikaioop/LogikaiOOP.java
@@ -1,14 +1,18 @@
 package logikaioop;
 
+import java.util.Scanner;
+
 public class LogikaiOOP {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Leírás");
         System.out.println("Az 1. állítás!");
         System.out.println("Az 2. állítás!");
         System.out.println("Az 3. állítás!");
         System.out.println("Melyik:");
+        sc.nextInt();
         System.out.println("A válasz jó|rossz");
     }
-    
+
 }
