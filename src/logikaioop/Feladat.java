675e7c1199e3eeb6acd434b6d236c019bb4a6039
DVMaja <majadreilinger@gmail.com>
2024-01-10 13:17:10 +0100

    A ládák állításai beállítva, kincs kiosztva, jól működik a kincs megtalálása, kiválasztása

--- src/logikaioop/Feladat.java
+++ src/logikaioop/Feladat.java
@@ -6,15 +6,18 @@ public class Feladat {
 
     private String leiras;
     private int melyik;
+    private Lada[] ladak;
 
     public Feladat(String leiras) {
-        Lada lada1 = new Lada("Az 1. állítás!", "", true);
-        Lada lada2 = new Lada("Az 2. állítás!", "", true);
-        Lada lada3 = new Lada("Az 3. állítás!", "", true);
-        ladakMegj(lada1, lada2, lada3);
-        isHelyes(lada1.isKincs());
-        this.melyik = melyik;
-        melyikAz();
+        ladak = new Lada[3];
+        ladak[0] = new Lada("Én rejtem a kincset!", "Arany", false);
+        ladak[1] = new Lada("Nem én rejtem a kincset!", "Ezüst", true);
+        ladak[2] = new Lada("Az arany hazudik!", "Bronz", false);
+        System.out.println(leiras);
+        ladakMegj(ladak[0], ladak[1], ladak[2]);
+        this.melyik = melyikAz();
+        System.out.println("Az válsztott ládába van kincs:" + isHelyes(ladak[this.melyik - 1].isKincs()));
+
     }
 
     public int melyikAz() {
@@ -29,8 +32,7 @@ public class Feladat {
     }
 
     private void ladakMegj(Lada lada1, Lada lada2, Lada lada3) {
-
-        System.out.println("Leírás\n" + lada1.getAllitas() + "\n" + lada2.getAllitas() + "\n" + lada3.getAllitas());
+        System.out.println(lada1.getAllitas() + "\n" + lada2.getAllitas() + "\n" + lada3.getAllitas());
     }
 
     private boolean isHelyes(boolean kincs) {
--- src/logikaioop/main.java
+++ src/logikaioop/main.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
-        new Feladat("Feladat");
+        new Feladat("Minden láda 1-1 állítást tartalmaz, csak az egyik az igaz.");
     }
 }
