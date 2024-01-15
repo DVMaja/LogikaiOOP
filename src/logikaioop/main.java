9e3f9d360f7068bd49f43c219ae727def37d5e30
DVMaja <majadreilinger@gmail.com>
2024-01-15 10:51:53 +0100

    ConsoleView kész

--- src/logikaioop/main.java
+++ src/logikaioop/main.java
@@ -1,7 +1,5 @@
 package logikaioop;
 
-import java.util.Scanner;
-
 public class main {
 
     public static void main(String[] args) {
--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -1,5 +1,27 @@
 package view;
 
+import logikaioop.Lada;
+
 public class ConsoleView {
-    
+
+    public ConsoleView() {
+    }
+
+    public void kiir(String kimeno) {
+        System.out.println(kimeno);
+    }
+
+    public void kiir(String kimeno, int szam) {
+        System.out.println(kimeno + szam);
+    }
+
+    public void kiir(String kimeno, boolean bool) {
+        System.out.println(kimeno + bool);
+    }
+
+    public void ladakMegj(Lada[] ladak) {
+        for (int i = 0; i < ladak.length; i++) {
+            System.out.println(ladak[i].getAllitas());
+        }
+    }
 }
