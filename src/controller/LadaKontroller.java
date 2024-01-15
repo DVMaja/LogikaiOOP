9f13705faadd0ed686b1d9b2e7a6a1dfe7c32bd0
Hidy97 <csaladi.alexandra@gmail.com>
2024-01-15 10:45:51 +0100

    Controller-be importálva a Láda model és a ConsoleView, leíró szöveg kirakva változóba

--- src/controller/LadaKontroller.java
+++ src/controller/LadaKontroller.java
@@ -1,6 +1,18 @@
 package controller;
 
+import logikaioop.Lada;
+import view.ConsoleView;
+
 public class LadaKontroller {
-    //Példányosítjuk view-t
+    //Adattagok létrehozása model(logikaioop) és view(ConsoleView)
+    private Lada model;
+    private ConsoleView consoleView;
+
+    public LadaKontroller() {
+        this.model = new Lada();
+        //"Minden láda 1-1 állítást tartalmaz, csak az egyik az igaz." ez megy a consoleview-ba
+        String leiras = "Minden láda 1-1 állítást tartalmaz, csak az egyik az igaz.";
+        this.consoleView = new ConsoleView();
+    }
     
 }
--- src/logikaioop/main.java
+++ src/logikaioop/main.java
@@ -1,10 +1,13 @@
 package logikaioop;
 
+import controller.LadaKontroller;
 import java.util.Scanner;
 
 public class main {
 
     public static void main(String[] args) {
-        new Feladat("Minden láda 1-1 állítást tartalmaz, csak az egyik az igaz.");
+        //new Feladat("Minden láda 1-1 állítást tartalmaz, csak az egyik az igaz.");
+        
+        new LadaKontroller();
     }
 }
